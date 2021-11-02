f08121cb3cdc9dc6be393c8ac855ba852496228f
kldaji <kldaji@naver.com>
2021-11-02 20:59:34 +0900

    #9 feat: drawer 헤더 추가 및 초대 여부 화면 이동

--- app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.FragmentHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
 
@@ -25,13 +28,27 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+        showBottomNavigation()
         setDrawerOpenListener()
+        setGroupAddClickListener()
+    }
+
+    private fun showBottomNavigation() {
+        requireActivity().findViewById<BottomNavigationView>(R.id.bottom_nav_main).visibility =
+            View.VISIBLE
     }
 
     private fun setDrawerOpenListener() {
         binding.toolbarHome.setNavigationOnClickListener {
             binding.drawerHome.open()
         }
+    }
+
+    private fun setGroupAddClickListener() {
+        binding.navViewDrawer.getHeaderView(0).findViewById<TextView>(R.id.tv_drawer_header_add)
+            .setOnClickListener {
+                it.findNavController().navigate(R.id.action_homeFragment_to_inviteCheckFragment)
+            }
     }
 
     override fun onDestroyView() {
