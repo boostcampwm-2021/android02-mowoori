40b9369e04c002cdfbecc03b4023f4221b69c5d4
good797979 <good79797979@naver.com>
2021-11-02 22:33:23 +0900

    #6 feat: Home Fragment에 눈내리는 애니메이션 추가

--- app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
@@ -1,20 +1,40 @@
 package com.ariari.mowoori.ui.home
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+<<<<<<< HEAD
 import android.widget.TextView
+=======
+import android.view.animation.AccelerateInterpolator
+import android.widget.ImageView
+>>>>>>> #6 feat: Home Fragment에 눈내리는 애니메이션 추가
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.FragmentHomeBinding
+<<<<<<< HEAD
 import com.google.android.material.bottomnavigation.BottomNavigationView
+=======
+import kotlin.random.Random
+>>>>>>> #6 feat: Home Fragment에 눈내리는 애니메이션 추가
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), Handler.Callback {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding ?: error(getString(R.string.binding_error))
+
+    private val SNOWING_MESSAGE_ID = 10
+    private val delayedSnowing: Handler = Handler(this@HomeFragment)
+    private var isSnowing = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +48,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+<<<<<<< HEAD
         showBottomNavigation()
         setDrawerOpenListener()
         setGroupAddClickListener()
@@ -49,10 +70,64 @@ class HomeFragment : Fragment() {
             .setOnClickListener {
                 it.findNavController().navigate(R.id.action_homeFragment_to_inviteCheckFragment)
             }
+=======
+        delayedSnowing.sendEmptyMessageDelayed(SNOWING_MESSAGE_ID, 100)
+>>>>>>> #6 feat: Home Fragment에 눈내리는 애니메이션 추가
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        isSnowing = false
+        delayedSnowing.removeCallbacksAndMessages(null)
         _binding = null
+    }
+
+    private fun makeSnow() = ImageView(requireContext()).apply {
+        setImageResource(R.drawable.ic_snow)
+        // snow 크기 설정
+        scaleX = Random.nextFloat() * .7f + .2f
+        scaleY = scaleX
+        // measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+    }
+
+    private fun dropSnow() {
+        val snow = makeSnow()
+        binding.containerHome.addView(snow)
+
+        // snow 좌표 설정
+        val snowStartHeight = snow.scaleY + 100f
+        val startX = Random.nextFloat() * binding.containerHome.width
+        val endX = Random.nextFloat() * binding.containerHome.width
+
+        val moverX = ObjectAnimator.ofFloat(snow, View.TRANSLATION_X, startX, endX)
+        val moverY = ObjectAnimator.ofFloat(
+            snow,
+            View.TRANSLATION_Y,
+            -snowStartHeight,
+            binding.containerHome.height + snowStartHeight
+        ).apply {
+            interpolator = AccelerateInterpolator(1f)
+        }
+
+        val set = AnimatorSet().apply {
+            playTogether(moverX, moverY)
+            duration = (Math.random() * 3000 + 3000).toLong()
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    _binding?.containerHome?.removeView(snow)
+                }
+            })
+        }
+
+        set.start()
+    }
+
+    override fun handleMessage(msg: Message): Boolean {
+        if (msg.what == SNOWING_MESSAGE_ID && isSnowing) {
+            dropSnow()
+            delayedSnowing.sendEmptyMessageDelayed(SNOWING_MESSAGE_ID, 100)
+        }
+        return true
     }
 }
