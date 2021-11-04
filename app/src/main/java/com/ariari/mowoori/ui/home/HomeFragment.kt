87e22bab5d5ddb2f4c30da0b43e3313527906e7d
kldaji <kldaji@naver.com>
2021-11-04 13:36:53 +0900

    #12 chore: 불필요한 import 제거

--- app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/home/HomeFragment.kt
@@ -11,21 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
-import com.ariari.mowoori.R
-import com.ariari.mowoori.databinding.FragmentHomeBinding
-import com.ariari.mowoori.util.TimberUtil
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.FragmentHomeBinding
 import com.ariari.mowoori.ui.home.adapter.DrawerAdapter
 import com.ariari.mowoori.ui.home.adapter.DrawerAdapterDecoration
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ariari.mowoori.util.TimberUtil
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
@@ -93,10 +88,10 @@ class HomeFragment : Fragment() {
     }
 
     private fun setObserver() {
-        viewModel.isSnowing.observe(viewLifecycleOwner, Observer {
+        viewModel.isSnowing.observe(viewLifecycleOwner, {
             updateSnowAnimation(it)
         })
-        viewModel.snowmanLevel.observe(viewLifecycleOwner, Observer {
+        viewModel.snowmanLevel.observe(viewLifecycleOwner, {
             updateSnowmanAnimation(it)
         })
     }
