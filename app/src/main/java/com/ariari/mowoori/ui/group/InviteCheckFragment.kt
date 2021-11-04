a46bc31c716770f1bd1590ab2b74a416d6679547
kldaji <kldaji@naver.com>
2021-11-04 13:49:25 +0900

    #12 feat: 초대 코드, 그룹 이름 입력 화면 이동 구현

--- app/src/main/java/com/ariari/mowoori/ui/group/InviteCheckFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/group/InviteCheckFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.FragmentInviteCheckBinding
 
@@ -24,6 +25,20 @@ class InviteCheckFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+        setNavigateToInviteCode()
+        setNavigateToGroupName()
+    }
+
+    private fun setNavigateToInviteCode() {
+        binding.btnInviteCheckYes.setOnClickListener {
+            it.findNavController().navigate(R.id.action_inviteCheckFragment_to_inviteCodeFragment)
+        }
+    }
+
+    private fun setNavigateToGroupName() {
+        binding.btnInviteCheckNo.setOnClickListener {
+            it.findNavController().navigate(R.id.action_inviteCheckFragment_to_groupNameFragment)
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/ariari/mowoori/ui/group/InviteCodeFragment.kt
@@ -0,0 +1,28 @@
+package com.ariari.mowoori.ui.group
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ariari.mowoori.R
+import com.ariari.mowoori.databinding.FragmentInviteCodeBinding
+
+class InviteCodeFragment : Fragment() {
+
+    private var _binding: FragmentInviteCodeBinding? = null
+    private val binding get() = _binding ?: error(getString(R.string.binding_error))
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentInviteCodeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
