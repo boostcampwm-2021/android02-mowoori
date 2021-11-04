b59ec85cd840c8576e3a93b69f11a568dce6af65
LimLight94 <chk705@gmail.com>
2021-11-04 16:52:50 +0900

    #2 chore: 어플리케이션 클래스 중복 삭제

--- app/src/main/java/com/ariari/mowoori/App.kt
+++ app/src/main/java/com/ariari/mowoori/App.kt
@@ -1,11 +1,13 @@
 package com.ariari.mowoori
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        FirebaseApp.initializeApp(this)
     }
 }
