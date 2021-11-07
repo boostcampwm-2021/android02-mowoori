7dbb401c059adf467abfcf79940decebdaa92126
LimLight94 <chk705@gmail.com>
2021-11-08 00:37:56 +0900

    #3 feat: 회원가입 Profile 이미지 추가 구현

--- app/src/main/java/com/ariari/mowoori/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/ariari/mowoori/ui/register/RegisterActivity.kt
@@ -2,13 +2,22 @@ package com.ariari.mowoori.ui.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.e
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.ActivityRegisterBinding
 import com.ariari.mowoori.ui.main.MainActivity
 import com.ariari.mowoori.util.EventObserver
+import com.ariari.mowoori.util.TimberUtil
 import com.ariari.mowoori.util.toastMessage
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,11 +27,17 @@ class RegisterActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityRegisterBinding.inflate(layoutInflater)
     }
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        it?.let {
+            viewModel.setProfileImage(it)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.viewModel = viewModel
+        binding.lifecycleOwner = this
         setObservers()
         viewModel.createNickName()
     }
@@ -30,6 +45,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun setObservers() {
         setInvalidNickNameObserver()
         setRegisterSuccessObserver()
+        setProfileClickObserver()
     }
 
     private fun setInvalidNickNameObserver() {
@@ -45,6 +61,12 @@ class RegisterActivity : AppCompatActivity() {
             } else {
                 toastMessage(getString(R.string.register_fail_msg))
             }
+        })
+    }
+
+    private fun setProfileClickObserver() {
+        viewModel.profileImageClickEvent.observe(this, EventObserver {
+            getContent.launch("image/*")
         })
     }
 
--- app/src/main/java/com/ariari/mowoori/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/ariari/mowoori/ui/register/RegisterViewModel.kt
@@ -1,16 +1,14 @@
 package com.ariari.mowoori.ui.register
 
+import android.net.Uri
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ariari.mowoori.data.repository.IntroRepository
+import com.ariari.mowoori.ui.register.entity.UserInfo
 import com.ariari.mowoori.util.Event
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,6 +24,12 @@ class RegisterViewModel @Inject constructor(
     private val _registerSuccessEvent = MutableLiveData<Event<Boolean>>()
     val registerSuccessEvent: LiveData<Event<Boolean>> = _registerSuccessEvent
 
+    private val _profileImageClickEvent = MutableLiveData<Event<Unit>>()
+    val profileImageClickEvent: LiveData<Event<Unit>> = _profileImageClickEvent
+
+    private val _profileImageUri = MutableLiveData<Uri>()
+    val profileImageUri: LiveData<Uri> = _profileImageUri
+
     fun createNickName() {
         viewModelScope.launch {
             val nickname = introRepository.getRandomNickName()
@@ -34,8 +38,11 @@ class RegisterViewModel @Inject constructor(
     }
 
     fun clickProfile() {
-        // TODO: 2021/11/04 이미지 가져오기
-        val imageUri = ""
+        _profileImageClickEvent.value = Event(Unit)
+    }
+
+    fun setProfileImage(uri: Uri) {
+        _profileImageUri.postValue(uri)
     }
 
     fun clickComplete() {
@@ -45,7 +52,16 @@ class RegisterViewModel @Inject constructor(
             return
         }
         viewModelScope.launch {
-            val success = introRepository.userRegister(nickname)
+            var uploadUrl = ""
+            profileImageUri.value?.let {
+                uploadUrl = introRepository.putUserProfile(it)
+            }
+            val success = introRepository.userRegister(
+                UserInfo(
+                    nickname = nickname,
+                    profileImage = uploadUrl
+                )
+            )
             _registerSuccessEvent.value = Event(success)
         }
     }
