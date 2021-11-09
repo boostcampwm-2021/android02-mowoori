2c064980618b1a12ffc1bd5ea8ef0a06dafe3f19
good797979 <good79797979@naver.com>
2021-11-09 15:07:53 +0900

    #23 fix: MissionsAddFragment.kt BaseFragment 적용

--- app/src/main/java/com/ariari/mowoori/ui/missions_add/MissionsAddFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/missions_add/MissionsAddFragment.kt
@@ -7,23 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.ariari.mowoori.R
+import com.ariari.mowoori.base.BaseFragment
 import com.ariari.mowoori.databinding.FragmentMissionsAddBinding
 
 
-class MissionsAddFragment : Fragment() {
-    private var _binding: FragmentMissionsAddBinding? = null
-    private val binding get() = _binding ?: error(getString(R.string.binding_error))
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_missions_add, container, false)
-        return inflater.inflate(R.layout.fragment_missions_add, container, false)
-    }
-
+class MissionsAddFragment : BaseFragment<FragmentMissionsAddBinding>(R.layout.fragment_missions_add) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
