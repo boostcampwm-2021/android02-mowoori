10b8772a08025b169af1cf13c9513cb245d11e7e
LimLight94 <chk705@gmail.com>
2021-11-09 23:47:35 +0900

    #17 feat: Group 로직 추가

--- app/src/main/java/com/ariari/mowoori/util/ToastMessageUtil.kt
+++ app/src/main/java/com/ariari/mowoori/util/ToastMessageUtil.kt
@@ -2,7 +2,12 @@ package com.ariari.mowoori.util
 
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 fun Context.toastMessage(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.toastMessage(msg: String){
+    this.requireContext().toastMessage(msg)
 }
