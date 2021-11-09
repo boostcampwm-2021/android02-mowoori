a699a9fe9b18a337fa463962bcf9b7cbb7970ed8
LimLight94 <chk705@gmail.com>
2021-11-09 16:28:21 +0900

    #17 fix: firebase userInfo 로직 coroutine 으로 수정

--- app/src/main/java/com/ariari/mowoori/ui/register/entity/User.kt
+++ app/src/main/java/com/ariari/mowoori/ui/register/entity/User.kt
@@ -6,7 +6,7 @@ data class User(
 )
 
 data class UserInfo(
-    val nickname: String,
-    val profileImage: String,
+    val nickname: String = "",
+    val profileImage: String = "",
     val groupList: List<String> = emptyList(),
 )
