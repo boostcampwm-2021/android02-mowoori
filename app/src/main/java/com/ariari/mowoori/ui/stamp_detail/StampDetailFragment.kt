9a4a8305eec10ba8b5f30c6d566ac244bf1f06a5
KLDAJI <kldaji@naver.com>
2021-11-10 13:00:21 +0900

    #24 feat: 스탬프 디테일 (인증/조회) 화면 생성

---
+++ app/src/main/java/com/ariari/mowoori/ui/stamp_detail/StampDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.ariari.mowoori.ui.stamp_detail
+
+import android.os.Bundle
+import android.view.View
+import com.ariari.mowoori.R
+import com.ariari.mowoori.base.BaseFragment
+import com.ariari.mowoori.databinding.FragmentStampDetailBinding
+
+class StampDetailFragment : BaseFragment<FragmentStampDetailBinding>(R.layout.fragment_stamp_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
