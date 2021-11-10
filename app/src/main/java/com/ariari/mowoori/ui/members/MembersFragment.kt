4dcd9040d07a9fcd6dae36c86d5936b9291b1fe8
LimLight94 <chk705@gmail.com>
2021-11-10 18:36:22 +0900

    #29 feat: MembersFragment BaseFragment 상속

--- app/src/main/java/com/ariari/mowoori/ui/members/MembersFragment.kt
+++ app/src/main/java/com/ariari/mowoori/ui/members/MembersFragment.kt
@@ -5,27 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.ariari.mowoori.R
+import com.ariari.mowoori.base.BaseFragment
 import com.ariari.mowoori.databinding.FragmentMembersBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MembersFragment : Fragment() {
-    private var _binding: FragmentMembersBinding? = null
-    private val binding get() = _binding ?: error(getString(R.string.binding_error))
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMembersBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class MembersFragment : BaseFragment<FragmentMembersBinding>(R.layout.fragment_members) {
+    private val viewModel: MembersViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
     }
 
     override fun onDestroyView() {
