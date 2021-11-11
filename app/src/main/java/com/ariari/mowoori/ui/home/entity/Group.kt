96e6fd5021ed3647d6c419d0a2ae67581aac38f2
LimLight94 <chk705@gmail.com>
2021-11-11 23:05:59 +0900

    #10 fix: DrawerAdapter 데이터 타입변경 GroupInfo -> Group

--- app/src/main/java/com/ariari/mowoori/ui/home/entity/Group.kt
+++ app/src/main/java/com/ariari/mowoori/ui/home/entity/Group.kt
@@ -3,11 +3,11 @@ package com.ariari.mowoori.ui.home.entity
 data class Group(
     val groupId: String,
     val groupInfo: GroupInfo,
+    var selected: Boolean = false,
 )
 
 data class GroupInfo(
     val groupName: String = "",
     val userList: List<String> = emptyList(),
     val missionList: List<String> = emptyList(),
-    var selected: Boolean = false,
 )
