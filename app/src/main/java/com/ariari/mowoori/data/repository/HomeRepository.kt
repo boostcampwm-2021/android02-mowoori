814a46aeacf356e4f75e8fe598118b8603bfc958
LimLight94 <chk705@gmail.com>
2021-11-11 23:05:09 +0900

    #10 feat: HomeRepository 로직
    - getGroup() 리턴타입 수정 Group -> GroupInfo
    - User 현재그룹아이디 갱신 로직 추가

--- app/src/main/java/com/ariari/mowoori/data/repository/HomeRepository.kt
+++ app/src/main/java/com/ariari/mowoori/data/repository/HomeRepository.kt
@@ -1,6 +1,6 @@
 package com.ariari.mowoori.data.repository
 
-import com.ariari.mowoori.ui.home.entity.GroupInfo
+import com.ariari.mowoori.ui.home.entity.Group
 import com.ariari.mowoori.ui.register.entity.UserInfo
 
 
@@ -9,5 +9,7 @@ interface HomeRepository {
 
     suspend fun getUserInfo(uid: String): Result<UserInfo>
 
-    suspend fun getGroupInfo(groupId: String): Result<GroupInfo>
+    suspend fun getGroup(groupId: String): Result<Group>
+
+    fun setCurrentGroupId(groupId: String)
 }
