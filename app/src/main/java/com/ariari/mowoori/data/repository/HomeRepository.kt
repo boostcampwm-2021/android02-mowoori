9d88a2063601236fc2f6001dd191a5c3ab10c330
LimLight94 <chk705@gmail.com>
2021-11-11 18:51:36 +0900

    #67 feat: HomeRepository group정보 api 추가

--- app/src/main/java/com/ariari/mowoori/data/repository/HomeRepository.kt
+++ app/src/main/java/com/ariari/mowoori/data/repository/HomeRepository.kt
@@ -1,5 +1,6 @@
 package com.ariari.mowoori.data.repository
 
+import com.ariari.mowoori.ui.home.entity.GroupInfo
 import com.ariari.mowoori.ui.register.entity.UserInfo
 
 
@@ -7,4 +8,6 @@ interface HomeRepository {
     fun getUserUid(): String?
 
     suspend fun getUserInfo(uid: String): Result<UserInfo>
+
+    suspend fun getGroupInfo(groupId: String): Result<GroupInfo>
 }
