6e902786c329aef56ae63178f12a3970fc95a8f9
Yunjoo Son <thsduswn4141@naver.com>
2021-11-16 16:11:45 +0900

    #18 feat: 구성원 목록 리사이클러뷰 어댑터 생성, empty 이미지 추가, Glide관련 바인딩 어댑터 생성

--- app/src/main/java/com/ariari/mowoori/util/BindingAdapters.kt
+++ app/src/main/java/com/ariari/mowoori/util/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.ariari.mowoori.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
@@ -20,6 +21,18 @@ object BindingAdapters {
             if (isCircle)
                 builder = builder.apply(RequestOptions.circleCropTransform())
             builder.into(imageView)
+        }
+    }
+
+    @BindingAdapter("setSrcFromUrl")
+    @JvmStatic
+    fun setSrcFromUrl(imageView: ImageView, url: String?) {
+        if (url == null) {
+            imageView.setImageResource(R.drawable.ic_empty)
+        } else {
+            Glide.with(imageView.context)
+                .load(url)
+                .into(imageView)
         }
     }
 
