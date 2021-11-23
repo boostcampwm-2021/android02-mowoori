6587cfbcfe607ff3b9db54599e69369af81e9ae2
KLDAJI <kldaji@naver.com>
2021-11-23 11:43:36 +0900

    #104 feat: 네트워크 콜백 해제 코드 추가

--- app/src/main/java/com/ariari/mowoori/MoWooriApp.kt
+++ app/src/main/java/com/ariari/mowoori/MoWooriApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
 import com.ariari.mowoori.util.NetworkCallBack
+import com.ariari.mowoori.util.toastMessage
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -17,8 +18,19 @@ class MoWooriApp : Application() {
         registerNetworkCallback()
     }
 
+    override fun onTerminate() {
+        super.onTerminate()
+        unRegisterNetworkCallback()
+    }
+
     private fun registerNetworkCallback() {
         (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
             .registerNetworkCallback(NetworkCallBack.networkRequest, NetworkCallBack)
+        NetworkCallBack.setDoCellularChanged { applicationContext.toastMessage("모바일 데이터에 연결되었습니다.") }
+    }
+
+    private fun unRegisterNetworkCallback() {
+        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+            .unregisterNetworkCallback(NetworkCallBack)
     }
 }
