d9ccdbf178b110d29f31134339ec10802789c146
Yunjoo Son <thsduswn4141@naver.com>
2021-12-03 01:44:20 +0900

    #159 fix: PictureDialogFragment 생성자 삭제 후 setFragmentResultListener 설정

--- app/src/main/java/com/ariari/mowoori/widget/PictureDialogFragment.kt
+++ app/src/main/java/com/ariari/mowoori/widget/PictureDialogFragment.kt
@@ -2,11 +2,13 @@ package com.ariari.mowoori.widget
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import com.ariari.mowoori.R
 import com.ariari.mowoori.databinding.DialogPictureBinding
 import com.ariari.mowoori.ui.stamp_detail.entity.PictureType
 
-class PictureDialogFragment(private val onClick: (pictureType: PictureType) -> Unit) :
+class PictureDialogFragment :
     BaseDialogFragment<DialogPictureBinding>(R.layout.dialog_picture) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,12 +17,26 @@ class PictureDialogFragment(private val onClick: (pictureType: PictureType) -> U
 
     private fun setClickListener() {
         binding.containerDialogPictureGallery.setOnClickListener {
-            onClick(PictureType.GALLERY)
+            setFragmentResult(
+                PICTURE_DIALOG,
+                bundleOf(
+                    PICTURE_DIALOG to PictureType.GALLERY
+                )
+            )
             dismiss()
         }
         binding.containerDialogPictureCamera.setOnClickListener {
-            onClick(PictureType.CAMERA)
+            setFragmentResult(
+                PICTURE_DIALOG,
+                bundleOf(
+                    PICTURE_DIALOG to PictureType.CAMERA
+                )
+            )
             dismiss()
         }
+    }
+
+    companion object {
+        const val PICTURE_DIALOG = "picture dialog"
     }
 }
